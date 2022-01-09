42761bbd556f3e1c14ad7b3a0f660ce829ebefd7
Sasha <aleks-one@ukr.net>
2022-01-09 23:07:59 +0200

    Skeleton without styles

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 const promise = fetch("http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no");
 
-let list = [];
-
 promise
     .then((response) => response.json())    
-    .then((data) => {
-        // list = data;  
+    .then((data) => {       
         console.log(data);
-        document.getElementById("conteiner").innerHTML = data.current;
-        // list.forEach(function (element) {
-        //     console.log(element);        
-        //     document.getElementById("conteiner").innerHTML
-        // });       
+        document.getElementById("city").innerHTML = data.location.name;
+        document.getElementById("country").innerHTML = data.location.country;
+        document.getElementById("date").innerHTML = data.location.localtime;
+        document.getElementById("condition").innerHTML = data.current.condition.text;
+        let image = data.current.condition.icon;
+        document.getElementById("icon").innerHTML = `<img src="http://${image}">`;
+        document.getElementById("temp").innerHTML = data.current.temp_c;
+        document.getElementById("feelslike").innerHTML = data.current.feelslike_c;
+        document.getElementById("humidity").innerHTML = data.current.humidity;
     });
     
     
